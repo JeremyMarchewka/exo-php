5d750c605c14fcd8bbf598455c2ad4dca5a06524
JeremyMarchewka <j.marchewka@simplon-charleville.fr>
2019-04-23 15:47:53 +0200

    question 1 à 6

---
+++ php.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+$a = "4";
+$b = "6";
+$c = "2";
+
+echo min($a, $b, $c);
+
+if ($a == $b)
+echo "a=b"
+
+$min ="5";
+$max ="100";
+echo rand($min, $max);
+
+
+$a="3";
+$b="8";
+echo ($a + $b)
+
+
+$a="20";
+$b="30";
+
+$gcd = gmp_gcd($a, $b);
+echo gmp_strval($gcd) . "\n";
+*/
+
+$min ="1";
+$max ="100";
+rand($min, $max);
+if rand = "50";
+echo "je vais faire du bon visuel"
+if rand <> "50";
+echo "je suis un pioupiou"
+
+?>
